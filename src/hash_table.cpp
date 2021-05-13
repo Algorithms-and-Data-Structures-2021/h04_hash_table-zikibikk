b2444dee1322793784cf32931fd3ca13a3cdd69a
Alina <zikibikk@gmail.com>
2021-05-13 23:18:50 +0300

    erm.. working version

--- src/hash_table.cpp
+++ src/hash_table.cpp
@@ -4,82 +4,104 @@
 
 namespace itis {
 
-  int HashTable::hash(int key) const {
-    return utils::hash(key, static_cast<int>(buckets_.size()));
-  }
-
-  HashTable::HashTable(int capacity, double load_factor) : load_factor_{load_factor} {
-    if (capacity <= 0) {
-      throw std::logic_error("hash table capacity must be greater than zero");
+    int HashTable::hash(int key) const {
+        return utils::hash(key, static_cast<int>(buckets_.size()));
     }
 
-    if (load_factor <= 0.0 || load_factor > 1.0) {
-      throw std::logic_error("hash table load factor must be in range [0...1]");
+    HashTable::HashTable(int capacity, double load_factor) : load_factor_{load_factor} {
+        if (capacity <= 0) {
+            throw std::logic_error("hash table capacity must be greater than zero");
+        }
+
+        if (load_factor <= 0.0 || load_factor > 1.0) {
+            throw std::logic_error("hash table load factor must be in range [0...1]");
+        }
+
+        buckets_.resize(capacity);
     }
 
-    // Tip: allocate hash-table buckets
-  }
-
-  std::optional<std::string> HashTable::Search(int key) const {
-    // Tip: compute hash code (index) and use linear search
-    return std::nullopt;
-  }
-
-  void HashTable::Put(int key, const std::string &value) {
-    // Tip 1: compute hash code (index) to determine which bucket to use
-    // Tip 2: consider the case when the key exists (read the docs in the header file)
-
-    if (static_cast<double>(num_keys_) / buckets_.size() >= load_factor_) {
-      // Tip 3: recompute hash codes (indices) for key-value pairs (create a new hash-table)
-      // Tip 4: use utils::hash(key, size) to compute new indices for key-value pairs
+    std::optional<std::string> HashTable::Search(int key) const {
+        for (auto elem : buckets_[hash(key)]) {
+            if (elem.first == key) {
+                return elem.second;
+            }
+        }
+        return std::nullopt;
     }
-  }
 
-  std::optional<std::string> HashTable::Remove(int key) {
-    // Tip 1: compute hash code (index) to determine which bucket to use
-    // TIp 2: find the key-value pair to remove and make a copy of value to return
-    return std::nullopt;
-  }
+    void HashTable::Put(int key, const std::string &value) {
+        for (auto &elem : buckets_[hash(key)]) {
+            if (elem.first == key) {
+                elem.second = value;
+                return;
+            }
+        }
 
-  bool HashTable::ContainsKey(int key) const {
-    // Note: uses Search(key) which is not initially implemented
-    return Search(key).has_value();
-  }
-
-  bool HashTable::empty() const {
-    return size() == 0;
-  }
-
-  int HashTable::size() const {
-    return num_keys_;
-  }
-
-  int HashTable::capacity() const {
-    return static_cast<int>(buckets_.size());
-  }
-
-  double HashTable::load_factor() const {
-    return load_factor_;
-  }
-
-  std::unordered_set<int> HashTable::keys() const {
-    std::unordered_set<int> keys(num_keys_);
-    for (const auto &bucket : buckets_) {
-      for (const auto &[key, _] : bucket) {
-        keys.insert(key);
-      }
+        buckets_[hash(key)].push_back(std::pair(key, value));
+        num_keys_++;
+        if (static_cast<double>(num_keys_) / buckets_.size() >= load_factor_) {
+            std::vector<Bucket> new_buckets = std::vector<Bucket>{};
+            new_buckets.resize(buckets_.size() * kGrowthCoefficient);
+            for (const auto &bucket : buckets_) {
+                for (const auto &elem : bucket) {
+                    auto new_index = utils::hash(elem.first, new_buckets.size());
+                    new_buckets[new_index].push_back(elem);
+                }
+            }
+            buckets_ = new_buckets;
+        }
     }
-    return keys;
-  }
 
-  std::vector<std::string> HashTable::values() const {
-    std::vector<std::string> values;
-    for (const auto &bucket : buckets_) {
-      for (const auto &[_, value] : bucket) {
-        values.push_back(value);
-      }
+    std::optional<std::string> HashTable::Remove(int key) {
+        for (const auto &elem : buckets_[hash(key)]) {
+            if (elem.first == key) {
+                auto val = elem.second;
+                buckets_[hash(key)].remove(elem);
+                num_keys_--;
+                return val;
+            }
+        }
+        return std::nullopt;
     }
-    return values;
-  }
+
+    bool HashTable::ContainsKey(int key) const {
+        return Search(key).has_value();
+    }
+
+    bool HashTable::empty() const {
+        return size() == 0;
+    }
+
+    int HashTable::size() const {
+        return num_keys_;
+    }
+
+    int HashTable::capacity() const {
+        return static_cast<int>(buckets_.size());
+    }
+
+    double HashTable::load_factor() const {
+        return load_factor_;
+    }
+
+    std::unordered_set<int> HashTable::keys() const {
+        std::unordered_set<int> keys(num_keys_);
+        for (const auto &bucket : buckets_) {
+            for (const auto &[key, _] : bucket) {
+                keys.insert(key);
+            }
+        }
+        return keys;
+    }
+
+    std::vector<std::string> HashTable::values() const {
+        std::vector<std::string> values;
+        for (const auto &bucket : buckets_) {
+            for (const auto &[_, value] : bucket) {
+                values.push_back(value);
+            }
+        }
+        return values;
+    }
 
 }  // namespace itis
